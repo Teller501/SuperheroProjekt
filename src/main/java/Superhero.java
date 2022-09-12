2e0605e382c1287eb94111c10396b572259bd65f
Teller501 <ande777j@stud.kea.dk>
2022-09-12 15:35:41 +0200

    added attributes and constructor in Superhero class

--- src/main/java/Superhero.java
+++ src/main/java/Superhero.java
@@ -1,2 +1,13 @@
 public class Superhero {
+    private String realName;
+    private int creationYear;
+    private String superPower;
+    private boolean isHuman;
+
+    public Superhero(String realName, int creationYear, String superPower, boolean isHuman){
+        this.realName = realName;
+        this.creationYear = creationYear;
+        this.superPower = superPower;
+        this.isHuman = isHuman;
+    }
 }
