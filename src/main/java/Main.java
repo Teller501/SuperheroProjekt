b26f4d0688e6f83645f72bd9f4671bc7c47b43e9
Teller501 <ande777j@stud.kea.dk>
2022-09-12 15:31:58 +0200

    added input for superhero realName, creationYear, superPower

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,19 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner keyb = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Opret en superhelt");
+
+        System.out.print("Indtast superheltens rigtige navn: ");
+        String realName = keyb.next();
+
+        System.out.print("Hvornår blev superhelten skabt? ");
+        int creationYear = keyb.nextInt();
+
+        System.out.print("Hvilket superkræfter besidder superhelten? ");
+        String superPower = keyb.next();
+
+
+    }
 }
